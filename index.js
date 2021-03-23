6358c50556d6b6e64a6a5a9834ca12a7b3035ac4
Tre' Seymour <wayne.seymour@elastic.co>
2021-03-23 16:22:55 -0600

    Woohoo, we can draw circles lol

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-var canvas = document.querySelector('#canvas');
-canvas.width = canvas.height = 500;
-
-var context = canvas.getContext('2d');
-// context.fillStyle = 'rgb(255, 192, 203)'; // rgb
-// context.fillStyle = 'rgba(255, 192, 203, 1)'; // rgba
-context.fillStyle = '#ffc0cb'; // hex
-context.fillRect(0, 0, canvas.width, canvas.height);
+var context = document.querySelector('#canvas').getContext('2d');
+context.beginPath();
+context.arc(50, 50, 20, Math.PI / 180 * 0, Math.PI / 180 * 360);
+context.fill();
+context.closePath();
+// context.stroke();
+// 1 radian = 57.2957795 degrees
+// radians = (Math.PI / 180) * degrees;
