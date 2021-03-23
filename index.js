f59f4d7eed73928a27ba3ce3e65386faa41e8db2
Tre' Seymour <wayne.seymour@elastic.co>
2021-03-23 16:43:17 -0600

    Now we have growing purple circles lol

--- index.js
+++ index.js
@@ -1,17 +1,55 @@
 var context = document.querySelector('#canvas').getContext('2d');
 // context.arc(centerX, centerY, radius, startAngle, endAngle, isAntiClockwise);
-
-context.beginPath();
-context.arc(120, 20, 10, 0, Math.PI * 2);
-context.fillStyle = 'rebeccaPurple'
-context.fill();
-context.closePath();
-
-context.beginPath();
-context.arc(20, 120, 10, 0, Math.PI * 2);
-context.lineWidth = 10;
-context.strokeStyle = 'pink'
-context.stroke();
-context.closePath();
 // 1 radian = 57.2957795 degrees
 // radians = (Math.PI / 180) * degrees;
+
+// context.beginPath();
+// context.arc(100, 70, 50, Math.PI / 180 * -90, 0);
+// context.fill()
+// context.closePath();
+
+
+
+
+
+
+
+
+
+let i = 0
+const loop = () => {
+  if (i++ < Math.PI * 2) {
+    drawPurpleCircle(i)
+  } else {
+    i = 0;
+    context.clearRect(0, 0, 500, 500)
+    setTimeout(loop, 400)
+  }
+}
+loop()
+function drawPurpleCircle(x) {
+  context.beginPath();
+  context.arc(100, 70, 50, 0, x)
+  context.fillStyle = 'rebeccaPurple'
+  context.fill()
+  context.closePath()
+  setTimeout(loop, 400)
+}
+
+
+
+
+
+
+
+// context.arc(120, 20, 10, 0, Math.PI * 2);
+// context.fillStyle = 'rebeccaPurple'
+// context.fill();
+// context.closePath();
+//
+// context.beginPath();
+// context.arc(20, 120, 10, 0, Math.PI * 2);
+// context.lineWidth = 10;
+// context.strokeStyle = 'pink'
+// context.stroke();
+// context.closePath();
