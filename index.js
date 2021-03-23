e4b746e05d7c41773f3c36b092dfc38bca266ae3
Tre' Seymour <wayne.seymour@elastic.co>
2021-03-23 16:31:27 -0600

    Woohoo, we can draw multiple circles and change their styles.

--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 var context = document.querySelector('#canvas').getContext('2d');
+// context.arc(centerX, centerY, radius, startAngle, endAngle, isAntiClockwise);
+
 context.beginPath();
-context.arc(50, 50, 20, Math.PI / 180 * 0, Math.PI / 180 * 360);
+context.arc(120, 20, 10, 0, Math.PI * 2);
+context.fillStyle = 'rebeccaPurple'
 context.fill();
 context.closePath();
-// context.stroke();
+
+context.beginPath();
+context.arc(20, 120, 10, 0, Math.PI * 2);
+context.lineWidth = 10;
+context.strokeStyle = 'pink'
+context.stroke();
+context.closePath();
 // 1 radian = 57.2957795 degrees
 // radians = (Math.PI / 180) * degrees;
